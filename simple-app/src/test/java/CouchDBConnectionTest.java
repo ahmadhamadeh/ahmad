07fb4c889a6dad73212e4b9c0231df021dc14527
Ahmad Hamadeh <ahmad.hamadeh@accenture.com>
2015-03-19 14:16:09 +0100

    Add a smoke test for couchdb

--- simple-app/src/test/java/CouchDBConnectionTest.java
+++ simple-app/src/test/java/CouchDBConnectionTest.java
@@ -1,19 +1,22 @@
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.lightcouch.CouchDbClient;
 import org.lightcouch.CouchDbProperties;
-import org.lightcouch.DocumentConflictException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class CouchDBConnectionTest {
-    @Test
-    public void connectionTest(){
+
+    public static final String TESTDB = "testdb";
+    private CouchDbClient dbClient;
+
+    @Before
+    public void setupFreshDB() {
         CouchDbProperties properties = new CouchDbProperties()
-                .setDbName("simple-app")
+                .setDbName(TESTDB)
                 .setCreateDbIfNotExist(true)
                 .setProtocol("https")
                 .setHost("couchdb40155-env-8900677.jelastic.elastx.net")
@@ -22,24 +25,28 @@ public class CouchDBConnectionTest {
                 .setPassword("admin")
                 .setMaxConnections(100)
                 .setConnectionTimeout(0);
+        dbClient = new CouchDbClient(properties);
+    }
 
-        CouchDbClient dbClient = new CouchDbClient(properties);
+    @After
+    public void tearDownDB() {
+        dbClient.context().deleteDB(TESTDB, "delete database");
+        dbClient.shutdown();
+    }
 
-        JsonObject jsonObject = dbClient.find(JsonObject.class, "123");
-        if(jsonObject == null) {
-            JsonObject json = new JsonObject();
+    @Test
+    public void connectionSmokeTest() {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("_id", "123");
+        jsonObject.addProperty("appname", "simple-app");
+        jsonObject.add("array", new JsonArray());
+        dbClient.save(jsonObject);
 
-            json.addProperty("_id", "123");
-            json.addProperty("appname", "simple-app");
-            json.add("array", new JsonArray());
-            dbClient.save(json);
-        }
         jsonObject = dbClient.find(JsonObject.class, "123");
-
         assertNotNull(jsonObject);
+        assertNotNull(jsonObject.get("_id"));
         assertNotNull(jsonObject.get("appname"));
         assertEquals(jsonObject.get("appname").getAsString(), "simple-app");
-        dbClient.shutdown();
-
+        dbClient.remove(jsonObject);
     }
 }
