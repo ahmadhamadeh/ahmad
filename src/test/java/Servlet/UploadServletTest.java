6d77773fb5ccffe231a2cd02007839599c7e630c
flekim13 <kim.flaethe@accenture.com>
2015-03-24 08:07:21 +0100

    Test cirllcllr

--- src/test/java/Servlet/UploadServletTest.java
+++ src/test/java/Servlet/UploadServletTest.java
@@ -1,24 +1,28 @@
 package Servlet;
 
+import Controller.DatabaseController;
 import org.junit.Before;
-import org.junit.Test;
+import org.lightcouch.CouchDbClient;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 public class UploadServletTest {
 
 
     private HttpServletResponse responseMock;
     private HttpServletRequest requestMock;
+    private CouchDbClient dbClientMock;
+    private DatabaseController dbControllerMock;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         responseMock = mock(HttpServletResponse.class);
         requestMock = mock(HttpServletRequest.class);
+        dbClientMock = mock(CouchDbClient.class);
+        dbControllerMock = mock(DatabaseController.class);
     }
-
 
 }
